f6b303e83947471f25fb035dcc06e059964b4c7f
Ruslan Yevdokymov <evruslan17@gmail.com>
2020-07-07 21:59:29 +0200

    Implement settings button

--- src/main/java/com/github/ruslanye/RankResolver/Controller/MainController.java
+++ src/main/java/com/github/ruslanye/RankResolver/Controller/MainController.java
@@ -1,4 +1,42 @@
 package com.github.ruslanye.RankResolver.Controller;
 
+import com.github.ruslanye.RankResolver.App;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class MainController {
+
+    public void showSettings() {
+        Stage stage = new Stage();
+        Parent root;
+        try {
+            root = FXMLLoader.load(App.class.getResource("View/Settings.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+        stage.setScene(new Scene(root));
+        stage.setTitle("Setup contest");
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.show();
+    }
+
+    public void settingsButtonClicked(MouseEvent event) {
+        if (event.getButton() == MouseButton.PRIMARY)
+            showSettings();
+    }
+
+    public void settingsButtonReleased(KeyEvent event) {
+        if (event.getCode() == KeyCode.ENTER)
+            showSettings();
+    }
 }
