88f5678267da15294cd3e43770d74069c9a30da9
Ruslan Yevdokymov <evruslan17@gmail.com>
2020-08-25 23:26:51 +0200

    Remove settings menu files

--- src/main/java/com/github/ruslanye/RankResolver/Controller/MainController.java
+++ src/main/java/com/github/ruslanye/RankResolver/Controller/MainController.java
@@ -7,36 +7,14 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class MainController {
-//    @FXML
-//    public void showSettings(ActionEvent event) {
-//        Stage stage = new Stage();
-//        Parent root;
-//        FXMLLoader loader = new FXMLLoader(App.class.getResource("View/Settings.fxml"));
-//        try {
-//            root = loader.load();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//            return;
-//        }
-//        stage.setScene(new Scene(root));
-//        stage.setTitle("Setup contest");
-//        stage.initModality(Modality.APPLICATION_MODAL);
-//        SettingsController controller = loader.getController();
-//        stage.setOnCloseRequest(e -> {
-//            if (controller.onClose())
-//                e.consume();
-//        });
-//        stage.show();
-//    }
 
     @FXML
-    public void startContest(ActionEvent event){
+    public void startContest(ActionEvent event) {
         Parent root;
         FXMLLoader loader = new FXMLLoader(App.class.getResource("View/Contest.fxml"));
         try {
